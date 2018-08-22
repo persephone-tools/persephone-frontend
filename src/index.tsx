e9dc6b9a18123d2eda766b4ec7148773b5c8873e
Aapeli <git@aapelivuorinen.com>
2018-08-22 17:28:21 +1000

    Add semantic UI

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'semantic-ui-css/semantic.min.css';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
