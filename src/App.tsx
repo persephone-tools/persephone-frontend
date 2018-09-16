545ca146a97b59313badef1718afb27104e64f51
Aapeli <git@aapelivuorinen.com>
2018-09-16 19:33:37 +1000

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import './App.css';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Index from './components/Index';
+
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <h1>Persephone frontend</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact={true} path="/" component={Index} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/Index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import { RouteComponentProps, withRouter } from 'react-router-dom'
+
+import { Button } from 'semantic-ui-react'
+
+class Index extends React.Component<RouteComponentProps<any>, React.Props<any>> {
+    public render() {
+        return (
+            <div>
+                <Button primary={true} onClick={() => this.props.history.push("/model/")}>See models</Button>
+            </div>
+        )
+    }
+}
+
+const routerIndex = withRouter(Index);
+
+export default routerIndex;
