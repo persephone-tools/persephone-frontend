3dbe45a7adf67ca715adfe329dea27fcb4ea5bee
Aapeli <git@aapelivuorinen.com>
2018-09-16 20:38:08 +1000

    Fix mistake in Corpus interface

--- src/interfaces/Corpus.tsx
+++ src/interfaces/Corpus.tsx
@@ -3,7 +3,7 @@ export default interface ICorpus {
     filesystem_path: string;
     id: number;
     label_type: string;
-    max_samples: string;
+    max_samples: number;
     name: string;
     preprocessed: boolean;
     testing: number[];
