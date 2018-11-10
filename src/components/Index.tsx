a3b4e8621a9cadd88789371fee1392f3c7a2a97b
Aapeli <git@aapelivuorinen.com>
2018-11-10 14:07:50 +1100

    Clean up homepage

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -2,18 +2,20 @@ import * as React from 'react';
 
 import { RouteComponentProps, withRouter } from 'react-router-dom'
 
-import { Button } from 'semantic-ui-react'
+import { Button, Container, Header, Icon } from 'semantic-ui-react'
 
 class Index extends React.Component<RouteComponentProps<any>, React.Props<any>> {
     public render() {
         return (
             <div>
-                <Button primary={true} onClick={() => this.props.history.push("/corpus/")}>See corpuses</Button>
-                <Button primary={true} onClick={() => this.props.history.push("/audio/")}>See audio</Button>
-                <Button primary={true} onClick={() => this.props.history.push("/utterance/")}>See utterances</Button>
-                <Button primary={true} onClick={() => this.props.history.push("/transcription/")}>See transcriptions</Button>
-                <Button primary={true} onClick={() => this.props.history.push("/label/")}>See labels</Button>
-                <Button primary={true} onClick={() => this.props.history.push("/model/")}>See models</Button>
+                <Container text={true} fluid={true}>
+                    <Header as='h1' icon={true} textAlign='center'>
+                        <Icon name='home' circular={true} />
+                        <Header.Content>Welcome to Persephone</Header.Content>
+                        <Button primary={true} onClick={() => this.props.history.push("/intro")}>Getting started</Button>
+                    </Header>
+                    <p>Welcome to the Persephone. Persephone is a suite of tools for Automatic Speech Recognition (ASR).</p>
+                </Container>
             </div>
         )
     }
