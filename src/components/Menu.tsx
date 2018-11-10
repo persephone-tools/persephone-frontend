6110616905a94939518ea020be10602d9d73534e
Aapeli <git@aapelivuorinen.com>
2018-11-10 13:49:59 +1100

    Fix indentation

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,15 +15,15 @@ export default class MainMenu extends React.Component {
                         <Menu.Item>
                             <Icon name='home' />
                             Home
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='info' />
                             Introduction
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='help' />
                             How-to
-                    </Menu.Item>
+                        </Menu.Item>
                     </Menu.Menu>
                 </Menu.Item>
                 <Menu.Item>
@@ -32,27 +32,27 @@ export default class MainMenu extends React.Component {
                         <Menu.Item>
                             <Icon name='box' />
                             Models
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='book' />
                             Corpuses
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='music' />
                             Audio
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='pencil alternate' />
                             Transcriptions
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='talk' />
                             Utterances
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='tag' />
                             Labels
-                    </Menu.Item>
+                        </Menu.Item>
                     </Menu.Menu>
                 </Menu.Item>
                 <Menu.Item>
@@ -61,15 +61,15 @@ export default class MainMenu extends React.Component {
                         <Menu.Item>
                             <Icon name='filter' />
                             Preprocess
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='hourglass start' />
                             Train
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item>
                             <Icon name='language' />
                             Transcribe
-                    </Menu.Item>
+                        </Menu.Item>
                     </Menu.Menu>
                 </Menu.Item>
                 <Menu.Item>
@@ -78,19 +78,19 @@ export default class MainMenu extends React.Component {
                         <Menu.Item href="https://lists.persephone-asr.org/postorius/lists/announce.lists.persephone-asr.org/" target='_blank'>
                             <Icon name='announcement' />
                             Announcements
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item href="https://lists.persephone-asr.org/postorius/lists/discuss.lists.persephone-asr.org/" target='_blank'>
                             <Icon name='discussions' />
                             Discussion
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item href="https://persephone-asr.org/" target='_blank'>
                             <Icon name='external' />
                             Website
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item href="https://github.com/persephone-tools/" target='_blank'>
                             <Icon name='github' />
                             GitHub
-                    </Menu.Item>
+                        </Menu.Item>
                     </Menu.Menu>
                 </Menu.Item>
                 <Menu.Item>
@@ -99,15 +99,15 @@ export default class MainMenu extends React.Component {
                         <Menu.Item href="https://lists.persephone-asr.org/" target='_blank'>
                             <Icon name='mail' />
                             Community mailing lists
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item href="https://persephone.readthedocs.io/en/latest/" target='_blank'>
                             <Icon name='book' />
                             Documentation
-                    </Menu.Item>
+                        </Menu.Item>
                         <Menu.Item href="https://www.customprogrammingsolutions.com/" target='_blank'>
                             <Icon name='dollar' />
                             Enterprise support
-                    </Menu.Item>
+                        </Menu.Item>
                     </Menu.Menu>
                 </Menu.Item>
                 <Menu.Item>
